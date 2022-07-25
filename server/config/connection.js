998495bf8b00b7fa3c03e5a73b694a2339fd0e5c
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-25 19:52:59 +1000

    added category model

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 // Add database connection mongoose and mongoDb
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_PATH, {
