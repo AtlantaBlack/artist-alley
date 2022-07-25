5347b8eee280fc336b6483d21b6ef2253abf02a7
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-25 21:48:15 +1000

    added User typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,19 @@
 // Add typeDefs for GraphQL
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    userType: String
+    username: String
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    birthday: String
+    orders: [Order]
+    products: [Product]
+  }
+`;
+
+module.exports = typeDefs;
