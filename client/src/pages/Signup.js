d616210ffd84c2823373e461ad34f3a36c55a8b3
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-27 19:08:15 +1000

    got container for asking user type to show/hide conditionally

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -14,84 +14,94 @@ function Signup(props) {
   // rendering form
   return (
     <div>
-      <div className="userType-container">
-        <h2>I am...</h2>
-        <button type="button" onClick={() => setUserType('Artist')}>
-          An Artist
-        </button>
-        <button type="button" onClick={() => setUserType('Non-Artist')}>
-          A Wizard
-        </button>
+      {/* if there is no user type defined, then show this first container that asks what type of user they are */}
+      {!userType && (
+        <div className="userType-container">
+          <h2>I am...</h2>
+          <button type="button" onClick={() => setUserType('Artist')}>
+            An Artist
+          </button>
+          <button type="button" onClick={() => setUserType('Non-Artist')}>
+            A Wizard
+          </button>
+        </div>
+      )}
+
+      {/* this here for testing the local state */}
+      <div>
+        <p>the user is a(n):</p>
+        {userType}
       </div>
 
-      {userType}
+      {/* if the user type is defined then show the sign up form */}
+      {userType && (
+        <div className="signup-container">
+          <h2>Enter your details</h2>
+          <form>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input
+                placeholder="Username"
+                name="username"
+                type="username"
+                id="username"
+              />
+            </div>
+            <div>
+              <label htmlFor="firstName">First name:</label>
+              <input
+                placeholder="First name"
+                name="firstName"
+                type="firstName"
+                id="firstName"
+              />
+            </div>
 
-      <div className="signup-container">
-        <h2>Enter your details</h2>
-        <form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              placeholder="Username"
-              name="username"
-              type="username"
-              id="username"
-            />
-          </div>
-          <div>
-            <label htmlFor="firstName">First name:</label>
-            <input
-              placeholder="First name"
-              name="firstName"
-              type="firstName"
-              id="firstName"
-            />
-          </div>
+            <div>
+              <label htmlFor="lastName">Last name:</label>
+              <input
+                placeholder="lastName"
+                name="lastName"
+                type="lastName"
+                id="lastName"
+              />
+            </div>
 
-          <div>
-            <label htmlFor="lastName">Last name:</label>
-            <input
-              placeholder="lastName"
-              name="lastName"
-              type="lastName"
-              id="lastName"
-            />
-          </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@email.com"
+                name="email"
+                type="email"
+                id="email"
+              />
+            </div>
 
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              placeholder="youremail@email.com"
-              name="email"
-              type="email"
-              id="email"
-            />
-          </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                placeholder="*****"
+                name="password"
+                type="password"
+                id="password"
+              />
+            </div>
 
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              placeholder="*****"
-              name="password"
-              type="password"
-              id="password"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="birthday">Birthday:</label>
-            <input
-              placeholder="DD/MM/YYYY"
-              name="birthday"
-              type="birthday"
-              id="birthday"
-            />
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+            <div>
+              <label htmlFor="birthday">Birthday:</label>
+              <input
+                placeholder="DD/MM/YYYY"
+                name="birthday"
+                type="birthday"
+                id="birthday"
+              />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
