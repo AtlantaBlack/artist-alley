0fb7f5136601632ab95f734b4447d0392f260d88
Jazzberries <anime.flick.chick@gmail.com>
2022-07-27 20:27:44 +1000

    Added user seeds

---
+++ server/seeders/seeds.js
@@ -0,0 +1,17 @@
+const db = require('../config/connection');
+const { User } = require('../models');
+const userSeeds = require('./userSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await User.deleteMany({});
+
+    await User.create(userSeeds);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log('Database seeded!');
+  process.exit(0);
+});
