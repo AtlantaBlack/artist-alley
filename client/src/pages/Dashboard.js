6ebde4b5a48a710ff899ad9e8cbb5803aa2156cf
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-28 21:47:24 +1000

    added basic add-post form

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -28,19 +28,55 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h2>posts</h2>
-      {/* map posts */}
-      {posts.map((post) => (
-        <div
-          key={post.id}
-          style={{ border: '1px solid green', margin: '10px 0' }}
-        >
-          <h3>{post.title}</h3>
-          <p>{post.description}</p>
-          <img src={post.image} alt={post.image} />
-          <p>posted by {post.user.username}</p>
-        </div>
-      ))}
+      <div className="add-post">
+        <h2>add a post</h2>
+        <form>
+          <div>
+            <label htmlFor="title">Post title:</label>
+            <input
+              placeholder="title of post"
+              name="title"
+              type="title"
+              id="title"
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              placeholder="description of post"
+              name="description"
+              type="description"
+              id="description"
+            ></textarea>
+          </div>
+
+          <div>
+            <label htmlFor="img">upload img:</label>
+            <button type="button">click to upload image</button>
+          </div>
+
+          <div>
+            <button type="button">Submit</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="posts">
+        <h2>posts</h2>
+        {/* map posts */}
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            style={{ border: '1px solid green', margin: '10px 0' }}
+          >
+            <h3>{post.title}</h3>
+            <p>{post.description}</p>
+            <img src={post.image} alt={post.image} />
+            <p>posted by {post.user.username}</p>
+            <p>likes:</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
