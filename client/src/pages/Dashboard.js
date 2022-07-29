5835559fb9634422b35775dc9c809e8172d4c829
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-29 19:27:54 +1000

    added delete previous posts to seeder; added id to post query

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_POSTS } from '../utils/queries';
 
 const Dashboard = () => {
-  const posts = [
-    {
-      id: 1,
-      title: 'this is a piece of crap',
-      description: 'piece of crap i said',
-      image: 'notworking.jpg',
-      postType: 'Portfolio',
-      user: {
-        id: '62e25dedb892551267e87606',
-        username: 'quinn'
-      }
-    },
-    {
-      id: 2,
-      title: 'hey hey hey',
-      description: 'fancy volleyball guy',
-      image: 'notworking2.jpg',
-      postType: 'Portfolio',
-      user: {
-        id: '62e25dedb892551267e87606',
-        username: 'quinn'
-      }
-    }
-  ];
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: 'this is a piece of crap',
+  //     description: 'piece of crap i said',
+  //     image: 'notworking.jpg',
+  //     postType: 'Portfolio',
+  //     user: {
+  //       id: '62e25dedb892551267e87606',
+  //       username: 'quinn'
+  //     }
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'hey hey hey',
+  //     description: 'fancy volleyball guy',
+  //     image: 'notworking2.jpg',
+  //     postType: 'Portfolio',
+  //     user: {
+  //       id: '62e25dedb892551267e87606',
+  //       username: 'quinn'
+  //     }
+  //   }
+  // ];
+
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+
+  console.log(posts);
 
   return (
     <div>
@@ -63,19 +70,25 @@ const Dashboard = () => {
 
       <div className="posts">
         <h2>posts</h2>
-        {/* map posts */}
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            style={{ border: '1px solid green', margin: '10px 0' }}
-          >
-            <h3>{post.title}</h3>
-            <p>{post.description}</p>
-            <img src={post.image} alt={post.image} />
-            <p>posted by {post.user.username}</p>
-            <p>likes:</p>
-          </div>
-        ))}
+
+        <div style={{ border: '1px solid orange' }}>
+          {loading ? (
+            <div> loading </div>
+          ) : (
+            posts.map((post) => (
+              <div
+                key={post._id}
+                style={{ border: '1px solid green', margin: '10px 0' }}
+              >
+                <h3>{post.title}</h3>
+                <p>{post.description}</p>
+                <img src={post.image} alt={post.image} />
+                <p>posted by {post.username}</p>
+                <p>likes:</p>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const QUERY_POSTS = gql`
   query getPosts {
     posts {
+      _id
       title
       description
       image
