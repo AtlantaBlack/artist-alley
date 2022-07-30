758e81804c045d09a39734143c5a20b2f830af7e
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-30 13:23:55 +1000

    added latest posts component

--- client/src/components/LatestPosts/index.js
+++ client/src/components/LatestPosts/index.js
@@ -1,9 +1,41 @@
 import './index.css';
 
 const LatestPosts = () => {
+  const posts = [
+    {
+      id: 1,
+      title: 'this is a piece of crap',
+      description: 'piece of crap i said',
+      image: 'notworking.jpg',
+      postType: 'Portfolio',
+      user: {
+        id: '62e25dedb892551267e87606',
+        username: 'quinn'
+      }
+    },
+    {
+      id: 2,
+      title: 'hey hey hey',
+      description: 'fancy volleyball guy',
+      image: 'notworking2.jpg',
+      postType: 'Portfolio',
+      user: {
+        id: '62e25dedb892551267e87606',
+        username: 'quinn'
+      }
+    }
+  ];
+
   return (
     <>
-      <p>Latest posts here</p>
+      <h2>Latest Posts</h2>
+      {posts.map((post) => (
+        <div style={{ border: '1px solid cyan' }}>
+          <h2>{post.title}</h2>
+          <p>by {post.user.username}</p>
+          <p>{post.description}</p>
+        </div>
+      ))}
     </>
   );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-import WelcomeScreen from '../components/WelcomeScreen';
+// import WelcomeScreen from '../components/WelcomeScreen';
+import LatestPosts from '../components/LatestPosts';
 
 const Home = () => {
   // if not logged in, render welcome screen:
-  return (
-    <div>
-      HOME PAGE
-      <WelcomeScreen />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     HOME PAGE
+  //     <WelcomeScreen />
+  //   </div>
+  // );
 
   // if logged in, render latest posts:
   // code here
+  return (
+    <div>
+      HOME PAGE
+      <LatestPosts />
+    </div>
+  );
 };
 
 export default Home;
