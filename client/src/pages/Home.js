b314ea6a2851a2f5c469dbf05f6fed79b27ad12e
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-31 17:38:27 +1000

    added conditional rendering to home based on loggedIn status

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import Auth from '../utils/auth';
 
-// import WelcomeScreen from '../components/WelcomeScreen';
+import WelcomeScreen from '../components/WelcomeScreen';
 import LatestPosts from '../components/LatestPosts';
 
 const Home = () => {
+  if (Auth.loggedIn()) {
+    return (
+      <div>
+        HOME PAGE
+        <LatestPosts />
+      </div>
+    );
+  }
   // if not logged in, render welcome screen:
-  // return (
-  //   <div>
-  //     HOME PAGE
-  //     <WelcomeScreen />
-  //   </div>
-  // );
-
-  // if logged in, render latest posts:
-  // code here
   return (
     <div>
       HOME PAGE
-      <LatestPosts />
+      <WelcomeScreen />
     </div>
   );
+
+  // if logged in, render latest posts:
+  // code here
 };
 
 export default Home;
